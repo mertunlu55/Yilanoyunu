e300d8e554ff65bdc03b1cb9e49b2dcf6072d81a
mertunlu55 <o244602012@giresun.edu.tr>
2025-12-17 13:35:37 +0300

    gitignore eklendi, ayarlar ve template güncellendi

--- projem/settings.py
+++ projem/settings.py
@@ -75,13 +75,14 @@ WSGI_APPLICATION = 'projem.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+# Database ayarları kaldırıldı - farklı bir database kullanılacak
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 
